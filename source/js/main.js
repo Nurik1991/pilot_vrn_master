55307df95cbe86ff828659a64302bb8e198490a4
Nurik1991 <iiepapx@gmail.com>
2021-02-03 16:04:07 +0300

    фыв
    фыв

--- source/js/main.js
+++ source/js/main.js
@@ -24,6 +24,10 @@ $(document).ready(function () {
 });
 
 
+// маска для телефонов
+$("[placeholder*='Тел']").mask("+7(999) 999-9999");
+
+
 
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -86,64 +90,60 @@ $(document).ready(function () {
 
 
 
-	// Get the modal
-	var modal = document.getElementById("myModal");
-	
-	// Get the button that opens the modal
-	var btn = document.getElementById("myBtn");
-	
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
-	
-	// When the user clicks the button, open the modal 
-	btn.onclick = function() {
-	  modal.style.display = "block";
-	}
-	
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	  modal.style.display = "none";
-	}
-	
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-	  if (event.target == modal) {
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+	modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
 		modal.style.display = "none";
-	  }
 	}
+}
 
 
 
-			// Get the modal
-			var modal = document.getElementById("myModal");
-		
-			// Get the button that opens the modal
-			var btn = document.getElementById("myBtn2");
-			
-			// Get the <span> element that closes the modal
-			var span = document.getElementsByClassName("close")[0];
-			
-			// When the user clicks the button, open the modal 
-			btn.onclick = function() {
-			  modal.style.display = "block";
-			}
-			
-			// When the user clicks on <span> (x), close the modal
-			span.onclick = function() {
-			  modal.style.display = "none";
-			}
-			
-			// When the user clicks anywhere outside of the modal, close it
-			window.onclick = function(event) {
-			  if (event.target == modal) {
-				modal.style.display = "none";
-			  }
-			}
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn2");
 
-// маска для телефонов
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-$("[placeholder*='Тел']").mask("+7(999) 999-9999");
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+	modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
 
 
 
