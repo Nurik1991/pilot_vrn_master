1cfbc6c4b55bb55c90fd69f9d476ced98e23d309
Nurik1991 <iiepapx@gmail.com>
2021-02-09 10:13:43 +0300

    порядок
    порядок

--- source/js/main.js
+++ source/js/main.js
@@ -1,31 +1,29 @@
- 
-
-
+// анимация при загрузке стр
 $(document).ready(function () {
 
-	setTimeout(function(){
+	setTimeout(function () {
 		show_first();
-	},500);
+	}, 500);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		show_second();
-	},2000);
+	}, 2000);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		show_third();
-	},3000);
+	}, 3000);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		show_fourth();
-	},4000);
+	}, 4000);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		show_fiveth();
-	},5000);
+	}, 5000);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		show_sixth();
-	},5500);
+	}, 5500);
 
 });
 
@@ -36,7 +34,7 @@ function show_first() {
 		opacity: '1'
 	});
 }
-function  show_second() {
+function show_second() {
 	$('.header').css({
 		visibility: 'visible',
 		top: '0px'
@@ -54,28 +52,24 @@ function show_third() {
 function show_fourth() {
 
 	$('.offer-swiper-pagination').css({
-		visibility: 'visible' ,
+		visibility: 'visible',
 		opacity: '1'
 	});
 }
 function show_fiveth() {
 
 	$('.search-block').css({
-		visibility: 'visible' ,
+		visibility: 'visible',
 		opacity: '1'
 	});
 }
 function show_sixth() {
 
 	$('.offer__bookmarks').css({
-		visibility: 'visible' ,
+		visibility: 'visible',
 		opacity: '1'
 	});
 }
-
-
-
-
 
 
 
@@ -112,8 +106,6 @@ $(document).ready(function () {
 $("[placeholder*='Тел']").mask("+7(999) 999-9999");
 
 // при клике на покупку перекрасится корзинка
-
-
 $('.card__buy-button').click(function (event) {
 	$('.offer__buy-button').css('background', '#ffa401');
 });
@@ -151,31 +143,21 @@ $(document).ready(function () {
 
 	// показать и скрыть отзывы
 	$('.reviews-title').click(function (event) {
-
 		if ($('.reviews-title').hasClass('open')) {
-
 			$(this).removeClass('open').addClass('close');
 			$('.reviews-body').slideUp();
-
 		}
 		else {
-
 			$(this).removeClass('close').addClass('open');
 			$('.reviews-body').slideDown();
 		}
-
-
 	});
 
-
+	// карточка товара сменить активное изображение
 	$('.description__choose-image img').click(function (event) {
 		let $this_img = $(this).attr('src')
 		$('.single-product__preview-image img').attr('src', $this_img);
-
 	});
-
-
-
 });
 
 
@@ -186,18 +168,14 @@ $(document).ready(function () {
 
 // Get the modal
 var modal = document.getElementById("myModal");
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks the button, open the modal 
-// btn.onclick = function () {
-// 	modal.style.display = "block";
-// }
-
+btn.onclick = function () {
+	modal.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
 	modal.style.display = "none";
@@ -209,8 +187,6 @@ window.onclick = function (event) {
 		modal.style.display = "none";
 	}
 }
-
-
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -222,9 +198,9 @@ var btn = document.getElementById("myBtn2");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-// btn.onclick = function () {
-// 	modal.style.display = "block";
-// }
+btn.onclick = function () {
+	modal.style.display = "block";
+}
 $('.callback').click(function (event) {
 	$('.modalwind').css('display', 'block');
 });
@@ -232,7 +208,6 @@ $('.callback').click(function (event) {
 span.onclick = function () {
 	modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
 	if (event.target == modal) {
@@ -242,20 +217,14 @@ window.onclick = function (event) {
 
 
 
-
-
 // Для моб меню
 $('.have-sub').click(function (event) {
-
-
 	if ($('.have-sub').children('.sub-menu').hasClass('active')) {
 		$(this).children('.sub-menu').removeClass('active');
 	}
 	else {
 		$(this).children('.sub-menu').addClass('active');
 	}
-
-
 });
 
 
